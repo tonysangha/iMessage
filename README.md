89a2835dcd88e02e39ed6febb02e5da1928629ec
Tony Sangha <tony.sangha@gmail.com>
2016-06-21 16:39:13 +1000

    no message

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# iMessage
-Backup iMessage Photos.
+# Backup iMessage
 
-For more information, visit: https://tonysangha.com/2015/11/20/backup-imessage-photos-using-a-python-script/
+Simple Python script made to save all your iMessage photos to a directory of
+your choosing. 
+
+For more information please visit my [Blog](https://tonysangha.com/2015/11/20/backup-imessage-photos-using-a-python-script/)
+
--- iMessage.py
+++ iMessage.py
@@ -1,7 +1,15 @@
-#!/usr/bin/python
-# Created Nov 2015 - Tony Sangha (tonysangha.com)
-# Test on OSX Yosemite and El Capitan
-# Tested with Python 2.7.10
+#!/usr/bin/env python
+
+"""
+    Written by Tony Sangha - Nov 2015
+    Github: https://github.com/tonysangha
+    Blog: http://tonysangha.com
+
+    * Script to save images received via iMessage on your MAC *
+
+    Tested on OSX Yosemite and El Capitan
+    Tested with Python 2.7.10
+"""
 
 import os
 import sys
@@ -18,26 +26,29 @@ non_exif_filenames = []  # Store FileNames without DateTime EXIF TAG
 # Path to iMessage directory, based upon User Log
 path = "/Users/" + os.getlogin() + "/Library/Messages/Attachments/"
 
-
 def md5(fname):
-    """ return HASH value for file """
+    """ 
+    return HASH value for file 
+    """
     hash = hashlib.md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash.update(chunk)
     return hash.hexdigest()
 
-
-def imageType(rawname):
-    """ Return the File Extension """
+def image_Type(rawname):
+    """ 
+    Return the File Extension 
+    """
     return os.path.splitext(rawname)[1]
 
-
-def renamePics(filename, rawName):
-    """ If the file contains the EXIF tags for DateTime, use tag to
+def rename_pics(filename, rawName):
+    """ 
+    If the file contains the EXIF tags for DateTime, use tag to
     rename picture, otherwise just return the original filename with a 
     random prefix to help in the event there are duplicate filenames but
-    hash value differs """
+    hash value differs 
+    """
     f = open(filename, 'rb')
     processed = exifread.process_file(f)
     stripped = str(processed.get('Image DateTime'))
@@ -46,50 +57,51 @@ def renamePics(filename, rawName):
         return '{2:02}-{1}-{0}_{03}.{04}.{05}'.format(dt.day, dt.month,
                                                       dt.year, dt.hour,
                                                       dt.minute, dt.second) \
-            + imageType(rawName)
+                                                      + imageType(rawName)
     else:
         return str(randint(0, 999999)) + "-" + rawName
 
-
-def populateDict(directory, diction):
-    """ Method injests a directiory and creates a dictionary to represent
+def populate_dict(directory, diction):
+    """ 
+    Method injests a directiory and creates a dictionary to represent
     the contents of the directory. The HASH value (unique) is used as the
-    dictionary key and filename and path as values associated to the key """
+    dictionary key and filename and path as values associated to the key 
+    """
     for root, dirs, files in os.walk(directory):
         for name in files:
             if str(name).endswith((".jpg", ".JPEG", ".JPG", ".PNG", ".png")):
-                newName = renamePics(os.path.abspath(os.path.join
+                newName = rename_pics(os.path.abspath(os.path.join
                                                      (directory, root, name)),
                                      name)
-                diction[md5(os.path.abspath(os.path.join(directory, root, name)))] \
-                    = name, \
+                diction[md5(os.path.abspath(os.path.join(directory, 
+                            root, name)))] = name, \
                     os.path.abspath(os.path.join(directory, root, name)), \
                     newName
     return diction
 
-
 def rename(oldName, newName, destination):
-    """ Rename the files in the destination """
+    """ 
+    Rename the files in the destination 
+    """
     return os.rename((destination) + "/" + str(oldName), (destination) + "/"
                      + str(newName))
 
-
-def copyFiles(destination):
-    """ Execute copy function of files """
+def copy_files(destination):
+    """ 
+    Execute copy function of files 
+    """
     for x in range(len(src_dict)):
         if src_dict.keys()[x] in dst_dict:
             pass
         else:
             shutil.copy(src_dict.values()[x][1], destination)
             rename(
-                src_dict.values()[x][0], src_dict.values()[x][2], destination)
-
+                   src_dict.values()[x][0], src_dict.values()[x][2], destination)
 
 def main(argv):
-    populateDict(path, src_dict)
-    populateDict(argv[1], dst_dict)
-    copyFiles(argv[1])
-
+    populate_dict(path, src_dict)
+    populate_dict(argv[1], dst_dict)
+    copy_files(argv[1])
 
 if __name__ == "__main__":
     main(sys.argv)
