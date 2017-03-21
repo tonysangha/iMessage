5b21194f733ebd744b544d110f033a66b875b601
Nicky Parseghian <atchoum2k@gmail.com>
2017-03-21 09:55:42 -0700

    Fix imageType function. Add Usage Description in comments

--- iMessage.py
+++ iMessage.py
@@ -9,6 +9,8 @@
 
     Tested on OSX Yosemite and El Capitan
     Tested with Python 2.7.10
+
+    usage: python iMessage.py <DestinationFolder>
 """
 
 import os
@@ -27,8 +29,8 @@ non_exif_filenames = []  # Store FileNames without DateTime EXIF TAG
 path = "/Users/" + os.getlogin() + "/Library/Messages/Attachments/"
 
 def md5(fname):
-    """ 
-    return HASH value for file 
+    """
+    return HASH value for file
     """
     hash = hashlib.md5()
     with open(fname, "rb") as f:
@@ -36,18 +38,18 @@ def md5(fname):
             hash.update(chunk)
     return hash.hexdigest()
 
-def image_Type(rawname):
-    """ 
-    Return the File Extension 
+def imageType(rawname):
+    """
+    Return the File Extension
     """
     return os.path.splitext(rawname)[1]
 
 def rename_pics(filename, rawName):
-    """ 
+    """
     If the file contains the EXIF tags for DateTime, use tag to
-    rename picture, otherwise just return the original filename with a 
+    rename picture, otherwise just return the original filename with a
     random prefix to help in the event there are duplicate filenames but
-    hash value differs 
+    hash value differs
     """
     f = open(filename, 'rb')
     processed = exifread.process_file(f)
@@ -62,10 +64,10 @@ def rename_pics(filename, rawName):
         return str(randint(0, 999999)) + "-" + rawName
 
 def populate_dict(directory, diction):
-    """ 
+    """
     Method injests a directiory and creates a dictionary to represent
     the contents of the directory. The HASH value (unique) is used as the
-    dictionary key and filename and path as values associated to the key 
+    dictionary key and filename and path as values associated to the key
     """
     for root, dirs, files in os.walk(directory):
         for name in files:
@@ -73,22 +75,22 @@ def populate_dict(directory, diction):
                 newName = rename_pics(os.path.abspath(os.path.join
                                                      (directory, root, name)),
                                      name)
-                diction[md5(os.path.abspath(os.path.join(directory, 
+                diction[md5(os.path.abspath(os.path.join(directory,
                             root, name)))] = name, \
                     os.path.abspath(os.path.join(directory, root, name)), \
                     newName
     return diction
 
 def rename(oldName, newName, destination):
-    """ 
-    Rename the files in the destination 
+    """
+    Rename the files in the destination
     """
     return os.rename((destination) + "/" + str(oldName), (destination) + "/"
                      + str(newName))
 
 def copy_files(destination):
-    """ 
-    Execute copy function of files 
+    """
+    Execute copy function of files
     """
     for x in range(len(src_dict)):
         if src_dict.keys()[x] in dst_dict:
